1dfd6096e5498228b08c8ee80dc3560ba8ac299a
shnumworldnum1 <77956842+shnumworldnum1@users.noreply.github.com>
2021-01-26 12:02:55 +0900

    Create 소스1.cpp

---
+++ 소스1.cpp
@@ -0,0 +1,17 @@
+/*
+제목: Hellow world 출력하기
+기능: 문자열의 출력
+파일이름: HellowComment.c
+ 수정날짜: 2014. 07. 15
+ */
+#include <stdio.h>	// 헤더파일 선언
+
+int main(void)	// main 함수의 시작
+{
+	/*
+	이 함수 내에서는 하나의 문자열을 출력한다.
+	문자열은 모니터로 출력된다.
+	*/
+	printf("Hellow world! \n");	// 문자열의 출력
+	return 0;	// 0의 반환
+}	// main 함수의 끝
