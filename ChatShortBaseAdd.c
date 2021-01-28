57d28e4d87a5710da5882e6291cf0ca62f1a971c
shnumworldnum1 <77956842+shnumworldnum1@users.noreply.github.com>
2021-01-28 16:32:12 +0900

    Update ChatShortBaseAdd.c

--- ChatShortBaseAdd.c
+++ ChatShortBaseAdd.c
@@ -5,8 +5,8 @@ int main(void)
 	char num1 = 1, num2 = 2, result1 = 0;
 	short num3 = 300, num4 = 400, result2 = 0;
 
-	printf("size of num1 & num2 %d \n", sizeof(num1), sizeof(num2));
-	printf("size of num3 & num4 %d \n", sizeof(num3), sizeof(num4));
+	printf("size of num1 & num2 %d %d \n", sizeof(num1), sizeof(num2));
+	printf("size of num3 & num4 %d %d \n", sizeof(num3), sizeof(num4));
 
 	printf("size of char add: %d \n", sizeof(num1 + num2));
 	printf("size of short add: %d \n", sizeof(num3 + num4));
